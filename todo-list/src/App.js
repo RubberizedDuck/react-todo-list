99338989ef8f986e59950320b2676f93a0af4fa6
RubberizedDuck <dyl_2536@hotmail.com>
2023-03-22 11:28:43 +1100

    First build

---
+++ todo-list/src/App.js
@@ -0,0 +1,13 @@
+import Heading from "./components/Heading";
+import Todo from "./components/Todo";
+
+function App() {
+	return (
+		<div>
+			<Heading />
+			<Todo />
+		</div>
+	);
+}
+
+export default App;
