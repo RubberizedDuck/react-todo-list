0134f2c8db1f5c858603be258b42fe481ae3248a
RubberizedDuck <dyl_2536@hotmail.com>
2023-03-22 11:30:33 +1100

    First build of Heading component

---
+++ todo-list/src/components/Heading.js
@@ -0,0 +1,13 @@
+import "./Heading.css";
+
+function Heading() {
+	return (
+		<div className="heading-container">
+			<div className="heading-container__heading">
+				<h1>Todo List</h1>
+			</div>
+		</div>
+	);
+}
+
+export default Heading;
