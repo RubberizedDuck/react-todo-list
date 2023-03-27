40a7fe895aa2b4e674f18b876d763fbb21d918b8
RubberizedDuck <dyl_2536@hotmail.com>
2023-03-27 15:55:55 +1100

    Incorporated states for more dynamic values

--- todo-list/src/components/Todo.js
+++ todo-list/src/components/Todo.js
@@ -3,16 +3,29 @@ import Textbox from "./Textbox";
 import Delete from "./Delete";
 import "./Todo.css";
 
-function Todo() {
+function Todo(props) {
+	function submittedTextBoxData(savedTextBoxData) {
+		const textBoxData = { ...savedTextBoxData, id: Math.random().toString() };
+		props.onAddTodo(textBoxData);
+	}
+
+	console.log(props.todoList);
+
 	return (
 		<div className="todo-container">
-			<div className="todo-list">
-				<Checkbox />
-				<Textbox />
-				<div>
-					<Delete />
+			<ul>
+				<div className="todo-list">
+					<li>
+						{props.todoList.map((todo) => (
+							<div className="todo-list__item">
+								<Checkbox />
+								<Textbox onSubmitTextBoxData={submittedTextBoxData} />
+								<Delete />
+							</div>
+						))}
+					</li>
 				</div>
-			</div>
+			</ul>
 		</div>
 	);
 }
