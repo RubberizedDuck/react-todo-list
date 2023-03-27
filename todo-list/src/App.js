983679b9c35870684d7aeb7400897f01e4c4cc1c
RubberizedDuck <dyl_2536@hotmail.com>
2023-03-27 15:54:33 +1100

    Add work with States

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
 import Heading from "./components/Heading";
 import Todo from "./components/Todo";
 
+const dummy_todos = [""];
+
 function App() {
+	const [todos, setTodos] = useState(dummy_todos);
+
+	function addTodoHandler(todo) {
+		setTodos((prevTodo) => {
+			return [todo, ...prevTodo];
+		});
+	}
 	return (
 		<div>
 			<Heading />
-			<Todo />
+			<Todo todoList={todos} onAddTodo={addTodoHandler} />
 		</div>
 	);
 }
