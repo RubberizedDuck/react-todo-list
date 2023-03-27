ce39972f57d442138e8aebdb49c9a9fdfa00e109
RubberizedDuck <dyl_2536@hotmail.com>
2023-03-27 15:55:18 +1100

    Incorporated states for dynamic values

--- todo-list/src/components/Textbox.js
+++ todo-list/src/components/Textbox.js
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import Delete from "./Delete";
 import "./Textbox.css";
 
-function Textbox() {
+function Textbox(props) {
+	const [textBox, setTextBox] = useState("");
+
+	function textBoxChanger(event) {
+		setTextBox(event.target.value);
+	}
+
+	function submitHandler(event) {
+		event.preventDefault();
+		const textBoxData = { text: textBox };
+		props.onSubmitTextBoxData(textBoxData);
+		setTextBox("");
+	}
 	return (
 		<div className="todo-input">
-			<form action="text">
+			<form action="text" onSubmit={submitHandler}>
 				<input
 					type="text"
+					value={textBox}
 					placeholder="Create todo"
-					className="SearchBox"
+					className="todoBox"
+					onChange={textBoxChanger}
 					required
 				/>
 			</form>
