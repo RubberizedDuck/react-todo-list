a8d27fc7a24804a79fcbff69e255aacfb809ad53
RubberizedDuck <dyl_2536@hotmail.com>
2023-03-28 11:56:46 +1100

    Remove value from input and updated state

--- todo-list/src/components/Textbox.js
+++ todo-list/src/components/Textbox.js
@@ -3,7 +3,7 @@ import Delete from "./Delete";
 import "./Textbox.css";
 
 function Textbox(props) {
-	const [textBox, setTextBox] = useState("");
+	const [textBox, setTextBox] = useState("Create Todo");
 
 	function textBoxChanger(event) {
 		setTextBox(event.target.value);
@@ -13,14 +13,13 @@ function Textbox(props) {
 		event.preventDefault();
 		const textBoxData = { text: textBox };
 		props.onSubmitTextBoxData(textBoxData);
-		setTextBox("");
+		setTextBox("Create Todo");
 	}
 	return (
 		<div className="todo-input">
 			<form action="text" onSubmit={submitHandler}>
 				<input
 					type="text"
-					value={textBox}
 					placeholder="Create todo"
 					className="todoBox"
 					onChange={textBoxChanger}
