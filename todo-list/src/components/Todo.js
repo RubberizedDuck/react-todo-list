873f5c1ffe59bfb0ed80c3feadcc9ba0f643b291
RubberizedDuck <dyl_2536@hotmail.com>
2023-03-28 11:58:57 +1100

    Add specific debugging and key id

--- todo-list/src/components/Todo.js
+++ todo-list/src/components/Todo.js
@@ -7,9 +7,10 @@ function Todo(props) {
 	function submittedTextBoxData(savedTextBoxData) {
 		const textBoxData = { ...savedTextBoxData, id: Math.random().toString() };
 		props.onAddTodo(textBoxData);
+		console.log("This is textBoxData var", textBoxData);
 	}
 
-	console.log(props.todoList);
+	console.log("This is props.todoList ", props.todoList);
 
 	return (
 		<div className="todo-container">
@@ -17,7 +18,7 @@ function Todo(props) {
 				<div className="todo-list">
 					<li>
 						{props.todoList.map((todo) => (
-							<div className="todo-list__item">
+							<div className="todo-list__item" key={todo.id}>
 								<Checkbox />
 								<Textbox onSubmitTextBoxData={submittedTextBoxData} />
 								<Delete />
