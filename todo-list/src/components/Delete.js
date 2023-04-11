7b31fc5b7678c9f2d0bb056654261f7ce52afb7e
RubberizedDuck <dyl_2536@hotmail.com>
2023-04-11 13:36:01 +1000

    Remove console log

--- todo-list/src/components/Delete.js
+++ todo-list/src/components/Delete.js
@@ -1,7 +1,6 @@
 import "./Delete.css";
 
 function Delete(props) {
-	console.log("This is the props in Delete.js", props);
 	return (
 		<div className="svg-cross" onClick={props.onDeleteHandler}>
 			<img
